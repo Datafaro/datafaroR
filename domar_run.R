2f81c1cb262b55d4c8b42a0dd185ae40c77f33d8
Daniel de la Rosa <drdsdaniel@gmail.com>
2020-10-29 11:15:52 -0400

    domar_run absolutes paths

--- domar_run.R
+++ domar_run.R
@@ -5,9 +5,9 @@ myEmail <- 'drdsdaniel@gmail.com'
 
 # Auth
 gs4_auth(
-  cache = '../.secrets',
+  cache = '/home/daniel/.secrets',
   email = 'drdsdaniel@gmail.com'
   )
 
 # imae_month
-secure_gss(myEmail, 'domar.email', 'imae_month', kvars = c('fecha', 'serie', 'Indice'))
+secure_gss(myEmail, '/home/daniel/domar/domar.email', 'imae_month', kvars = c('fecha', 'serie', 'Indice'))
