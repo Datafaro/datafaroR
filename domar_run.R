48956a00019b89e6b88618869adb6fc47e72b7b8
Daniel de la Rosa <drdsdaniel@gmail.com>
2020-10-29 10:52:13 -0400

    auth with cache

--- domar_run.R
+++ domar_run.R
@@ -1,10 +1,13 @@
 library(domar)
 library(googlesheets4)
 
-email <- 'drdsdaniel@gmail.com'
+myEmail <- 'drdsdaniel@gmail.com'
 
 # Auth
-gs4_auth('drdsdaniel@gmail.com') # Cualquier email con acceso a la lista domar.
+gs4_auth(
+  cache = '../.secrets',
+  email = 'drdsdaniel@gmail.com'
+  )
 
 # imae_month
-secure_gss(email, 'domar.email', 'imae_month', kvars = c('fecha', 'serie', 'Indice'))
+secure_gss(myEmail, 'domar.email', 'imae_month', kvars = c('fecha', 'serie', 'Indice'))
