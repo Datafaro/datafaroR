9c3d5d7aecc9fbc19c6587080f0b253c14b18184
Daniel de la Rosa <drdsdaniel@gmail.com>
2020-10-29 12:26:55 -0400

    select user for test server

--- domar_run.R
+++ domar_run.R
@@ -1,13 +1,23 @@
+
+print(Sys.time()) # Para el log del cron.
+
 library(domar)
 library(googlesheets4)
 
 myEmail <- 'drdsdaniel@gmail.com'
 
+# Esto es porque tengo users distintos en la máquina de prueba y el servidor.
+user <- if(file.exists('/home/drdsdaniel/domar/domar_run.R')){
+  'drdsdaniel'
+}else{
+  'daniel'
+}
+
 # Auth
 gs4_auth(
-  cache = '/home/daniel/.secrets',
+  cache = paste0('/home/', user, '/.secrets'),
   email = 'drdsdaniel@gmail.com'
   )
 
 # imae_month
-secure_gss(myEmail, '/home/daniel/domar/domar.email', 'imae_month', kvars = c('fecha', 'serie', 'Indice'))
+secure_gss(myEmail, paste0('/home/', user,'/domar/domar.email'), 'imae_month', kvars = c('fecha', 'serie', 'Indice'))
